29fe2667bbb77b4a2b05f34555bd6803a475b2e9
kekuso <kekuso808@gmail.com>
2016-07-06 21:37:28 -1000

    Implented creating page from POST data.

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ function handleRequest(request, response) {
   // GET method
   if(request.method == 'GET') {
     url = request.url.toString();
-    filename = url.replace('localhost', '');
+    filename = url.replace('localhost', '').toLowerCase();
 
     if(filename === '/') {
       filename = 'index.html';
@@ -48,19 +48,56 @@ function handleRequest(request, response) {
     });
 
     request.on('data', function(chunk) {
-      postData = querystring.parse(chunk);
+      postData = chunk.toString();
+      // var parsed = querystring.parse(request.url);
+      // console.log("Parsed query string: " + parsed);
       response.writeHead(200);
       response.write(chunk);
 
-      var elementName = getElementName(chunk);
+      createNewPage(postData);
       response.end();
     });
   }
 
-  function getElementName(chunk) {
-    var index = chunk.indexOf('&');
-    return chunk.slice(11, index);
+  function createNewPage(postData) {
+    var elementName;
+    var elementSymbol;
+    var elementAtomicNumber;
+    var elementDescription;
+    var pairs = [];
+    var queryArray = postData.split('&');
+    for(var i = 0; i < queryArray.length; i++) {
+      pairs[i] = queryArray[i].split('=');
+    }
+
+    elementName = pairs[0][1];
+    elementSymbol = pairs[1][1];
+    elementAtomicNumber = pairs[2][1];
+    elementDescription = pairs[3][1];
+    editedDescription = elementDescription.replace(/\+/g, " ");
+
+    var newHTMLPage =  '<!DOCTYPE html>\n' +
+                       '<html lang="en">\n' +
+                       '<head>\n' +
+                         '<meta charset="UTF-8">\n' +
+                         '<title>The Elements - '+ elementName +'</title>\n' +
+                         '<link rel="stylesheet" href="/css/styles.css">\n' +
+                       '</head>\n' +
+                       '<body>\n' +
+                         '<h1>'+ elementName + '</h1>\n' +
+                         '<h2>'+ elementSymbol + '</h2>\n' +
+                         '<h3>Atomic number '+ elementAtomicNumber + '</h3>\n' +
+                         '<p>' + editedDescription + '</p>\n' +
+                         '<p><a href="/">back</a></p>\n' +
+                       '</body>\n' +
+                       '</html>';
+
+    fs.writeFile("./public/" + elementName.toLowerCase() + ".html", newHTMLPage, function (err) {
+      if (err) throw err;
+      console.log("New file created.");
+    });
   }
+
   request.on('end', function () {
     console.log('No more data.');
     server.close();
@@ -79,47 +116,3 @@ server.on('clientError', function(err, socket) {
   socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
 });
 
-// server.on('request', function (request, response) {
-
-// });
-
-// fs.writeFile("./public", "Boron", function (err) {
-//   if (err) throw err;
-//   console.log("file saved.");
-// });
-
-
-// var postData = querystring.stringify({
-//   'msg' : 'Hello World!'
-// });
-
-// var options = {
-//   hostname: 'localhost',
-//   port: port,
-//   path: '/',
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/x-www-form-urlencoded',
-//     'Content-Length': Buffer.byteLength(postData)
-//   }
-// };
-
-// var req = http.request(options, function(res) {
-//   console.log('STATUS: ' + res.statusCode);
-//   console.log('HEADERS: ' + res.headers.toString());
-//   res.setEncoding('utf8');
-//   res.on('data', function(chunk) {
-//     console.log('BODY: ' + chunk);
-
-//   });
-//   res.on('end', function () {
-//     console.log('No more data in response.');
-//   });
-// });
-
-// req.on('error', function(e) {
-//   console.log('problem with request: ' + e);
-// });
-
-// req.write(postData);
-// req.end();
