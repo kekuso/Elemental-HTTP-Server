45cae7102f05e9e57ac7cdeb9feee3b034574e4e
kekuso <kekuso808@gmail.com>
2016-07-06 18:23:52 -1000

    HTTP server gives access to index, helium, hydrogen, 404, and css files

--- server.js
+++ server.js
@@ -1,12 +1,39 @@
 var http = require ('http');
+var fs = require('fs');
 var port = 8080;
+var filename;
+var url;
 
 var server = http.createServer(handleRequest);
 
 function handleRequest(request, response) {
-  response.end('Path hit: ' + request.url + '\n' +
-                'Socket: ' + request.socket.toString() + '\n' +
-                'Authentication details: ');
+  url = request.url.toString();
+  filename = url.replace('localhost', '');
+
+  if(filename === '/') {
+    filename = '/public/index.html';
+  }
+
+  fs.exists('.' + filename, function(exists) {
+    if(exists) {
+      console.log("file path: " + filename);
+      fs.readFile('.' + filename, function (err, messageBody) {
+        if(err) throw err;
+        console.log(response.statusCode);
+        response.writeHead(200);
+        response.write('\n' + messageBody);
+        response.end();
+      });
+    }
+    else {
+      fs.readFile('./public/404.html', function (err, messageBody) {
+        if(err) throw err;
+        response.writeHead(404);
+        response.write('\n' + messageBody);
+        response.end();
+      });
+    }
+  });
 }
 
 server.listen(port, function () {
@@ -15,4 +42,8 @@ server.listen(port, function () {
 
 server.on('clientError', function(err, socket) {
   socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
+
+server.on('request', function (request, response) {
+
 });
